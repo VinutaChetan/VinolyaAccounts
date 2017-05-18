8d74bb8c91803aeb6a2f8fa7eecf6913282b6e13
Chetan Agali <chetand.agali@gmail.com>
2017-05-18 14:52:09 +0530

    add charts

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,6 +7,7 @@ class Transaction < ActiveRecord::Base
 	after_create :add_acc_balance
 
 	def self.list_view
+
 		transaction=Transaction.order('transaction_date ASC').first
 		start_year=transaction.transaction_date.year
 		end_year=Date.today.year
@@ -36,5 +37,101 @@ class Transaction < ActiveRecord::Base
 			account.save
 		end
 	end		
+
+	def self.list_cr_amount
+		#ordering of transaction date in ascending order and taking the first object
+		transaction=Transaction.order('transaction_date ASC').first
+		#getting the start_year as a first object year
+		start_year=transaction.transaction_date.year
+		#getting  end_year as a todays year
+		end_year=Date.today.year
+		#take on array of months 
+    	months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+		#take one empty hash
+		data = {}
+		data_debit={}
+		cr_dr={}
+		#check condition that first year date should be less than or equal to todats year
+		while(start_year<=end_year)
+			#empty hash
+			data_month = {}
+			data_mon_debit={}
+			#itterating over an array months
+			months.each_with_index do |month, index|
+				#where start date is start_year-01-1 
+				query_start_date = Date.parse("#{start_year}-#{index + 1}-1")
+				#where end date is start_year-01-1 end of this year
+				query_end_date = query_start_date.end_of_month
+				
+				#putting key as month and value as transaction  sum of amount between start_year-01-1 and start_year-01-1 end of this year 
+				data_month[month] = Transaction.where('transaction_date >= ? AND transaction_date <= ? AND transaction_type=?' ,query_start_date, query_end_date,"credit").sum(:amount)
+			
+				data_mon_debit[month] = Transaction.where('transaction_date >= ? AND transaction_date <= ? AND transaction_type=?' ,query_start_date, query_end_date,"debit").sum(:amount)
+				
+			end
+			#putting key as start_year and value as monthwise transactions strat_year as a key and datamonth contains key as month and value as monthwise transactions
+			data[start_year] = [data_month]
+			data_debit[start_year] = [data_mon_debit]
+			
+			#year should be increse to next index
+			start_year = start_year + 1
+		end
+		# cr_dr={}
+		# types=["credit","debit"]
+		# cr_dr[types]=
+		return data
+		return data_debit
+		#return cr_dr
+	end	
+
+
+	def self.list_dr_amount
+		#ordering of transaction date in ascending order and taking the first object
+		transaction=Transaction.order('transaction_date ASC').first
+		#getting the start_year as a first object year
+		start_year=transaction.transaction_date.year
+		#getting  end_year as a todays year
+		end_year=Date.today.year
+		#take on array of months 
+    	months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+		#take one empty hash
+		data = {}
+		data_debit={}
+		
+		#check condition that first year date should be less than or equal to todats year
+		while(start_year<=end_year)
+			#empty hash
+			data_month = {}
+			data_mon_debit={}
+			#itterating over an array months
+			months.each_with_index do |month, index|
+				#where start date is start_year-01-1 
+				query_start_date = Date.parse("#{start_year}-#{index + 1}-1")
+				#where end date is start_year-01-1 end of this year
+				query_end_date = query_start_date.end_of_month
+				
+				#putting key as month and value as transaction  sum of amount between start_year-01-1 and start_year-01-1 end of this year 
+				data_month[month] = Transaction.where('transaction_date >= ? AND transaction_date <= ? AND transaction_type=?' ,query_start_date, query_end_date,"credit").sum(:amount)
+			
+				data_mon_debit[month] = Transaction.where('transaction_date >= ? AND transaction_date <= ? AND transaction_type=?' ,query_start_date, query_end_date,"debit").sum(:amount)
+				
+			end
+			#putting key as start_year and value as monthwise transactions strat_year as a key and datamonth contains key as month and value as monthwise transactions
+			#data[start_year] = [data_month]
+			data_debit[start_year] = [data_mon_debit]
+			
+			#year should be increse to next index
+			start_year = start_year + 1
+		end
+		# cr_dr={}
+		# types=["credit","debit"]
+		# cr_dr[types]=
+		#return data
+		return data_debit
+		#return cr_dr
+		
+
+	end	
+
 
 end
