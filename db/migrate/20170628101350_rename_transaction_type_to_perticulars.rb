d170c9a7398d0fa85a3044316c17ede5d1849249
Chetan Agali <chetand.agali@gmail.com>
2017-07-04 10:42:02 +0530

    adding migrations and restoring data

--- db/migrate/20170628101350_rename_transaction_type_to_perticulars.rb
+++ db/migrate/20170628101350_rename_transaction_type_to_perticulars.rb
@@ -1,5 +1,5 @@
-class RenameTransactionTypeToPerticulars < ActiveRecord::Migration
-  def change
-  	rename_column :perticulars,:transaction_type,:perticular_kind
-  end
-end
+# class RenameTransactionTypeToPerticulars < ActiveRecord::Migration
+#   def change
+#   	rename_column :perticulars,:transaction_type,:perticular_kind
+#   end
+# end
