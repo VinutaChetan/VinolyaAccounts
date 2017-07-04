14f1cf8cb18625f4b90b17ca4b6c50cd1fd982a7
Chetan Agali <chetand.agali@gmail.com>
2017-07-04 10:49:02 +0530

    adding migrations and restoring data

--- db/migrate/20170628103301_remove_transaction_type_from_perticulars.rb
+++ db/migrate/20170628103301_remove_transaction_type_from_perticulars.rb
@@ -1,5 +1,5 @@
-# class RemoveTransactionTypeFromPerticulars < ActiveRecord::Migration
-#   def change
-#   	remove_column :perticulars,:transaction_type
-#   end
-# end
+class RemoveTransactionTypeFromPerticulars < ActiveRecord::Migration
+  def change
+  	# remove_column :perticulars,:transaction_type
+  end
+end
