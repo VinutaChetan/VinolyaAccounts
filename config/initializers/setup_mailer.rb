9356c83dfc99b856bcb28b7ad7d3bc95f3408fcb
Chetan Agali <chetand.agali@gmail.com>
2017-08-21 16:55:23 +0530

    transaction radio buttons selected automatically using jquery

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
    :domain => "gmail.com",
    :authentication => "plain",
    :enable_starttls_auto => true,
-   :user_name => "vinuta.udagatti@gmail.com",
-   :password => "VinutaChetan",
+   :user_name => "rortraining.welt@gmail.com",
+   :password => "secret123#@!",
    :openssl_verify_mode  => 'none'
 }
